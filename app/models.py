6a48f50629f73e375d9e72da0f59e2b5217dbf52
Marvin Chomba <marvin.chomba24@gmail.com>
2020-04-26 23:37:39 +0300

    create source and article classes

---
+++ app/models.py
@@ -0,0 +1,25 @@
+class Source:
+  '''
+  Source class to define Source objects
+  '''
+
+  def __init__(self, id, name, description, url, category, language, country):
+    self.id = id
+    self.name = name
+    self.decription = description
+    self.url = url
+    self.category = category
+    self.language = language
+    self.country = country
+
+
+class Article:
+  '''
+  Source class to define Article objects
+  '''
+
+  def __init__(self, author, title, publishedAt, url):
+    self.author = author
+    self.title = title
+    self.publishedAt = publishedAt
+    self.url = url
