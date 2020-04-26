021e908efdfd1fadaf83f1e02ce06d27aa0b216e
Marvin Chomba <marvin.chomba24@gmail.com>
2020-04-27 00:28:44 +0300

    add changes to articles view

--- app/main/views.py
+++ app/main/views.py
@@ -13,3 +13,14 @@ def index():
   title = "News-App"
 
   return render_template('index.html', title=title, sources=sources)
+
+
+@main.route('/article/<sourceId>')
+def articles(sourceId):
+  '''
+  function that displays top stories from a particular source
+  '''
+  articles = get_top_articles(sourceId)
+  title = f"{sourceId} articles"
+
+  return render_template("articles.html", title=title, articles=articles)
